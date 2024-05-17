f72d52fdc1b9ae83dd3b399251c495f72e958816
ChloePrime <chloe_von_einzbern@qq.com>
2024-05-17 16:07:42 +0800

    1.20.1-1.1.0

--- fabric/src/main/java/mod/chloeprime/hitfeedback/client/fabric/HitFeedbackClientFabric.java
+++ fabric/src/main/java/mod/chloeprime/hitfeedback/client/fabric/HitFeedbackClientFabric.java
@@ -1,10 +1,10 @@
 package mod.chloeprime.hitfeedback.client.fabric;
 
+import fuzs.forgeconfigapiport.api.config.v2.ForgeConfigRegistry;
 import mod.chloeprime.hitfeedback.HitFeedbackMod;
 import mod.chloeprime.hitfeedback.client.ClientConfig;
 import mod.chloeprime.hitfeedback.client.HitFeedbackClient;
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraftforge.api.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 
 /**
@@ -14,7 +14,7 @@ public class HitFeedbackClientFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HitFeedbackClient.init();
-        ModLoadingContext.registerConfig(HitFeedbackMod.MOD_ID, ModConfig.Type.CLIENT, ClientConfig.SPEC);
+        ForgeConfigRegistry.INSTANCE.register(HitFeedbackMod.MOD_ID, ModConfig.Type.CLIENT, ClientConfig.SPEC);
         HitFeedbackClient.setup();
     }
 }
